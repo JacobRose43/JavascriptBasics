6486e194b8bad38bea61de2795e0a92aae9cfd6b
JacobRose <jacobrose.433@gmail.com>
2022-01-26 22:57:32 +0100

    update

---
+++ data/script.js
@@ -0,0 +1,9 @@
+// var, const and let!!!
+
+// var - globally scoped and function scoped, can be updated and re-declared*
+// const - globally and function scoped, cannot be updated or re-declared
+// let - block scoped, can be updated but not re-declared
+
+//re-declared - nadpisana
+
+//NaN - not a number
