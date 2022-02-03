bd9b1b91a5b8aad2ea5aab3ebc08593bacf63340
JacobRose <95928931+JacobRose43@users.noreply.github.com>
2022-02-03 20:42:52 +0100

    update

--- data/script.js
+++ data/script.js
@@ -137,4 +137,65 @@ for (let i = 0; i < order.ratings.length; i++) {
 	console.log(order.ratings[i]);
 }
 
-//for in  for objects, learn!!!!!!!!!!!!!!!!
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+//NEW FOR LOOPS
+
+//FOR OF - strings, arrays
+
+const seatingChart = [
+	["Kristen", "Erik", "Namita"],
+	["Kristen2", "Erik2", "Namita2"],
+	["Kristen3", "Erik3", "Namita3"],
+];
+
+for (let row of seatingChart) {
+	for (let students of row) {
+		console.log(students);
+	}
+}
+
+console.log("\n================================\n");
+
+for (let e of "Example text") {
+	console.log(e);
+}
+
+//FOR IN - objects
+
+const testScores = {
+	keenan: 80,
+	damon: 67,
+	kim: 89,
+	shawn: 91,
+	kdzan: 40,
+	daeyon: 88,
+	kaazm: 59,
+	shuttler: 21,
+};
+
+console.log("\n================================\n");
+
+for (let person in testScores) {
+	console.log(`${person} scored: ${testScores[person]}`);
+}
+
+console.log("\n================================\n");
+
+//avg score
+
+let total = 0;
+let scores = Object.values(testScores);
+
+for (let score of scores) {
+	total += score;
+}
+console.log(total / scores.length);
+
+//Object. method
+
+console.log("\n\nObject Keys:\n" + Object.keys(testScores));
+console.log("\n\nObject Values:\n" + Object.values(testScores));
+console.log("\n\nObject Entries:\n" + Object.entries(testScores));
+
+console.log("\n================================\n");
